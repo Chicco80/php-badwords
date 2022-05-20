7325bc236f656e8973614956d1f567962a1dd488
Chicco80 <chicco80lisa@gmail.com>
2022-05-20 16:06:46 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$stringa = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae corrupti alias eveniet nihil quam perspiciatis optio cumque debitis dicta quasi.';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <p><?php echo $stringa ?></p>
+    <p><?php echo strlen($stringa) ?></p>
+</body>
+</html>
