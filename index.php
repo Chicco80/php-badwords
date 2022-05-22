bae063e2f79367ba75131da667e52bd95ba60ba1
Chicco80 <chicco80lisa@gmail.com>
2022-05-22 11:21:54 +0200

    modifiche

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
  <?php
-$stringa = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea nostrum, quam quaerat inventore incidunt maiores optio exercitationem omnis aliquid amet quos deserunt sapiente eligendi beatae corporis voluptates! Ducimus fugit similique ipsa voluptate sint tempora totam repellendus quasi enim! Nobis placeat autem iure in velit illum quia cum voluptatem, dignissimos doloremque dolorem veniam officiis voluptatibus reiciendis quis aut aperiam quidem, vero doloribus odit animi ad assumenda atque. Id earum est, fugiat saepe eligendi necessitatibus quae animi sed rem quam quas maiores at perspiciatis repellat? Repellat doloremque officiis sapiente consectetur earum. Laboriosam, accusantium. Tenetur sed incidunt amet labore aliquid! Nesciunt suscipit beatae nisi doloremque adipisci modi aut tempore illum quia aliquid laborum quisquam minima praesentium, earum officia quas unde ipsum animi. Eius necessitatibus fugiat at sunt corrupti sit! Asperiores eum earum aperiam ipsam. Quis ipsa et natus dignissimos praesentium error asperiores sit sequi eveniet reiciendis quos voluptatem repudiandae dolor dolores, ullam est!';
 
+$testo='Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, inventore culpa! Dolores, deleniti minus. Commodi pariatur doloribus magnam enim ratione porro corporis delectus molestiae in? Earum, explicabo? Dicta veniam quidem amet? Eius quas dolores possimus, ratione eos neque, impedit a fuga deserunt voluptas quasi? Itaque totam quis asperiores eos soluta accusamus obcaecati doloremque saepe amet repellat eaque ut officia atque deserunt vel, at cum quos voluptatum ratione aliquid dolore dolorum natus quisquam sequi. Quam nostrum est placeat dicta molestias? Aperiam dolores nemo nulla cumque error enim impedit, iste ipsa dicta officia eius dolorem distinctio cum fuga saepe doloremque necessitatibus culpa!';
 if(isset($_GET['parola'])){
-        $parolacensurata = $_GET['parola'];
-        $testoCambiato = str_replace($parolacensurata,'***', $stringa);
-    }else{
-        $testoCambiato = 'La parola non è presente nel testo';
-    }
+    $testoModificato = str_replace($_GET['parola'], '***',strtolower($testo));
+}else {
+    $testoModificato= 'Parola non presente nel testo';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,17 +15,18 @@ if(isset($_GET['parola'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>php-badwords</title>
 </head>
 <body>
-    <h2>Testo</h2>
-    <p><?php echo $stringa ?></p>
-    <p>Lunghezza stringa: <?php echo strlen($stringa) ?></p>
-    <h1>Nuova Stringa con parola censurata</h1>
-    <p><?php echo($testoCambiato) ?></p>
-
+    <h1>Testo</h1>
+    <p> <?php echo $testo ?></p>
+    <p>Il testo è lungo:  <?php echo strlen($testo) ?></p>
+    <h2>Il testo modificato è:</h2>
+    <p><?php echo ucfirst($testoModificato) ?></p>
+    
+    
 </body>
-</html> 
+</html>
 
-<?php
+
 
